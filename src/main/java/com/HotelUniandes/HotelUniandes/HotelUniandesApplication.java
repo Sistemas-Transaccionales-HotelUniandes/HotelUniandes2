c41285a0f15083ae3ddbb3905da3bc409da3db5f
Tomas Rodriguez <t.rodriguezp@uniandes.edu.co>
2023-11-30 12:17:53 -0500

    Inicio Hotel2.0

---
+++ src/main/java/com/HotelUniandes/HotelUniandes/HotelUniandesApplication.java
@@ -0,0 +1,13 @@
+package com.HotelUniandes.HotelUniandes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelUniandesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelUniandesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/HotelUniandes/HotelUniandes/HotelUniandesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HotelUniandes.HotelUniandes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelUniandesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
